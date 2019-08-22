dd17becf8e27f254a8241630cbef3522db347aa8
leadrian <thewishwithin@gmail.com>
2019-08-22 20:54:00 +0200

    Add Junit 5 params dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ subprojects {
 
     dependencies {
         testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api", version = junitVersion)
+        testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-params", version = junitVersion)
         testImplementation(group = "org.assertj", name = "assertj-core", version = "3.12.1")
 
         testRuntimeOnly(group = "org.junit.jupiter", name = "junit-jupiter-engine", version = junitVersion)
