b40ff077b1569bad6347de02b818afe6989dd52d
leadrian <thewishwithin@gmail.com>
2019-08-23 18:45:49 +0200

    Merge HashTest and HashImplTest

--- equalizer-core/src/test/java/ch/leadrian/equalizer/HashImplTest.java
+++ equalizer-core/src/test/java/ch/leadrian/equalizer/HashImplTest.java
@@ -2,14 +2,11 @@ package ch.leadrian.equalizer;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-class HashImplTest implements HashTest {
-
-    @Override
-    public <T> HashBuilder<T> newBuilder() {
-        return new HashImpl.Builder<>();
-    }
+class HashImplTest {
 
     @Test
     void builderShouldBuildHashImpl() {
@@ -17,5 +14,86 @@ class HashImplTest implements HashTest {
 
         assertThat(hash)
                 .isInstanceOf(HashImpl.class);
+    }
+
+
+    @Test
+    void givenValueIsNullItShouldReturnZero() {
+        Hash<TestData> hash = new HashImpl.Builder<TestData>().build();
+
+        int result = hash.hashCode(null);
+
+        assertThat(result)
+                .isZero();
+    }
+
+    @Test
+    void givenNoHashStepsItShouldReturnSystemIdentityHashCode() {
+        TestData testData = new TestData("foo", new Object[0], 1337);
+        Hash<TestData> hash = new HashImpl.Builder<TestData>().build();
+
+        int result = hash.hashCode(testData);
+
+        assertThat(result)
+                .isEqualTo(System.identityHashCode(testData));
+    }
+
+    @Test
+    void shouldComputeHash() {
+        String stringValue = "foo";
+        Object[] arrayValue = new Object[]{"test"};
+        int intValue = 1337;
+        TestData testData = new TestData(stringValue, arrayValue, intValue);
+        Hash<TestData> hash = new HashImpl.Builder<TestData>()
+                .withSuper(new HashImpl.Builder<BaseTestData>().hash(BaseTestData::getIntValue).build())
+                .hash(TestData::getStringValue)
+                .hashDeep(TestData::getArrayValue)
+                .hashIdentity(TestData::getStringValue)
+                .build();
+
+        int result = hash.hashCode(testData);
+
+        assertThat(result)
+                .isEqualTo(31 *
+                        (31 *
+                                (31 *
+                                        (31 +
+                                                (31 + Integer.hashCode(intValue))
+                                        ) + stringValue.hashCode()
+                                ) + Arrays.deepHashCode(arrayValue)
+                        ) + System.identityHashCode(stringValue));
+    }
+
+    private static class BaseTestData {
+
+        private final int intValue;
+
+        BaseTestData(int intValue) {
+            this.intValue = intValue;
+        }
+
+        int getIntValue() {
+            return intValue;
+        }
+    }
+
+    private static class TestData extends BaseTestData {
+
+        private final String stringValue;
+        private final Object[] arrayValue;
+
+        TestData(String stringValue, Object[] arrayValue, int intValue) {
+            super(intValue);
+            this.stringValue = stringValue;
+            this.arrayValue = arrayValue;
+        }
+
+        String getStringValue() {
+            return stringValue;
+        }
+
+        Object[] getArrayValue() {
+            return arrayValue;
+        }
     }
 }
