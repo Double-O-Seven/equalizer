42c9709acb308bfdf859bce59337e57a020ba48e
leadrian <thewishwithin@gmail.com>
2019-08-25 01:25:36 +0200

    Added more Javadoc

--- equalizer-core/src/main/java/ch/leadrian/equalizer/Equalizer.java
+++ equalizer-core/src/main/java/ch/leadrian/equalizer/Equalizer.java
@@ -1,19 +1,41 @@
 package ch.leadrian.equalizer;
 
+/**
+ * Class containing factory methods for builders.
+ *
+ * @see EqualsBuilder
+ * @see HashCodeBuilder
+ * @see EqualsAndHashCodeBuilder
+ */
 public final class Equalizer {
 
     private Equalizer() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * @param targetClass The class for which {@link Equals} should be used to implement {@link Object#equals(Object)}
+     * @param <T>         Type of {@code targetClass}
+     * @return {@link EqualsBuilder} for class {@code targetClass}
+     */
     public static <T> EqualsBuilder<T> equalsBuilder(Class<T> targetClass) {
         return new EqualsImpl.Builder<>(targetClass);
     }
 
+    /**
+     * @param <T> Type of the class for which {@link HashCode} should be used to implement {@link Object#hashCode()}
+     * @return {@link EqualsBuilder} for type {@code T}
+     */
     public static <T> HashCodeBuilder<T> hashCodeBuilder() {
         return new HashCodeImpl.Builder<>();
     }
 
+    /**
+     * @param targetClass The class for which {@link EqualsAndHashCode} should be used to
+     *                    implement {@link Object#equals(Object)} and {@link Object#hashCode()}
+     * @param <T>         Type of {@code targetClass}
+     * @return {@link EqualsAndHashCodeBuilder} for class {@code targetClass}
+     */
     public static <T> EqualsAndHashCodeBuilder<T> equalsAndHashCodeBuilder(Class<T> targetClass) {
         return new EqualsAndHashCodeImpl.Builder<>(targetClass);
     }
--- equalizer-core/src/main/java/ch/leadrian/equalizer/EqualsAndHashCode.java
+++ equalizer-core/src/main/java/ch/leadrian/equalizer/EqualsAndHashCode.java
@@ -1,4 +1,54 @@
 package ch.leadrian.equalizer;
 
+/**
+ * <p>
+ * Combines {@link Equals} and {@link HashCode}. The combined interfaces guarantees
+ * that equality checks and hashing are configured in a way such that the contracts between
+ * {@link Object#equals(Object)} and {@link Object#hashCode()} are followed, meaning that if two objects are
+ * equivalent, then their hash codes are the same.
+ * A specific instance may be configured and instantiated using {@link Equalizer#equalsAndHashCodeBuilder(Class)} (Class)}.
+ * </p>
+ * Example:
+ * <pre>
+ *     public class Person {
+ *
+ *         private static final EqualsAndHashCode&lt;Person&gt; EQUALS_AND_HASH_CODE = Equalizer.equalsAndHashCodeBuilder(Person.class)
+ *                 .compare(Person::getAge)
+ *                 .compareAndHash(Person::getName)
+ *                 .build();
+ *
+ *         private final int age;
+ *         private final String name;
+ *
+ *         public Person(int age, String name) {
+ *             this.age = age;
+ *             this.name = name;
+ *         }
+ *
+ *         public int getAge() {
+ *             return age;
+ *         }
+ *
+ *         public String getName() {
+ *             return name;
+ *         }
+ *
+ *         &#64;Override
+ *         public int hashCode() {
+ *             return EQUALS_AND_HASH_CODE.hashCode(this);
+ *         }
+ *
+ *         &#64;Override
+ *         public boolean equals(Object object) {
+ *             return EQUALS_AND_HASH_CODE.equals(this, object);
+ *         }
+ *     }
+ * </pre>
+ *
+ * @param <T> Type for which {@link EqualsAndHashCode} has been configured.
+ * @see HashCode
+ * @see Equals
+ * @see EqualsAndHashCodeBuilder
+ */
 public interface EqualsAndHashCode<T> extends Equals<T>, HashCode<T> {
 }
